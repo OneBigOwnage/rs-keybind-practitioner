b9ce26ce126208f5a7129433711db6d9b7fe59be
Niek van den Bos <niekvandenbos@hotmail.com>
2021-07-19 23:55:21 +0200

    Use the components in the app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Delay } from './Delay';
+import { GlobalCooldown } from './GlobalCooldown';
+import { GlobalCooldownDataService } from './services/GlobalCooldownDataService';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'rs-keybind-sandbox';
+
+  constructor(public dataService: GlobalCooldownDataService) { }
+
+  addGcd() {
+    let id = Array.from(crypto.getRandomValues(new Uint8Array(20)), dec => dec.toString(16).padStart(2, "0")).join('');
+
+    this.dataService.add(new GlobalCooldown(id));
+  }
+
+  waitTicks(number: number) {
+    let id = Array.from(crypto.getRandomValues(new Uint8Array(20)), dec => dec.toString(16).padStart(2, "0")).join('');
+
+    this.dataService.add(new Delay(id, number));
+  }
+
+  isGlobalCooldown(gcdOrDelay: GlobalCooldown | Delay): gcdOrDelay is GlobalCooldown {
+    return gcdOrDelay instanceof GlobalCooldown;
+  }
+
+  isDelay(gcdOrDelay: GlobalCooldown | Delay): gcdOrDelay is Delay {
+    return gcdOrDelay instanceof Delay;
+  }
+
 }
