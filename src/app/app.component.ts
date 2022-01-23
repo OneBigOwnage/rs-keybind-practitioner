fd87c5bc8244e47abf9941a0de244e8deb8fc000
OneBigOwnage <niekvandenbos@hotmail.com>
2022-01-23 19:58:46 +0100

    Add method to load rotation into memory.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import GameLoop from './runescape/GameLoop';
+import RotationRepository from './runescape/RotationRepository';
+import { Tick } from './runescape/Tick';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   showRotationBuilder: boolean = false;
 
-  constructor(public game: GameLoop) { }
+  constructor(public game: GameLoop, public repo: RotationRepository) { }
+
+  ngOnInit() {
+    this.loadRotation();
+  }
 
   start() {
     this.game.start();
@@ -18,5 +24,13 @@ export class AppComponent {
 
   stop() {
     this.game.stop();
+  }
+
+  loadRotation() {
+    this.repo.rotation.next([
+      new Tick(Date.now(), ['A'], []),
+      new Tick(Date.now(), ['B', 'B'], []),
+      new Tick(Date.now(), ['C'], []),
+    ]);
   }
 }
