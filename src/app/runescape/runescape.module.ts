ffb4f7848971b75c32b390dba17d7f0a730a97b9
OneBigOwnage <niekvandenbos@hotmail.com>
2022-01-23 13:44:23 +0100

    Export the tick component from runescape module

--- src/app/runescape/runescape.module.ts
+++ src/app/runescape/runescape.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TickComponent } from './tick/tick.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TickComponent
+  ],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [TickComponent]
 })
 export class RunescapeModule { }
