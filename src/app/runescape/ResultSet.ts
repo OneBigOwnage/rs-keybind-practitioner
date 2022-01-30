13ac7fbe9fba52f41c0042310b6c73c6099a1967
Niek van den Bos <niekvandenbos@hotmail.com>
2022-01-30 23:16:37 +0100

    Add ResultSet model.

--- src/app/runescape/ResultSet.ts
+++ src/app/runescape/ResultSet.ts
@@ -1,3 +1,9 @@
 export default class ResultSet {
-
+  constructor(
+    public readonly ID: string,
+    public correctKeyPresses: string[],
+    public incorrectKeyPresses: string[],
+    public correctClicks: { x: number, y: number }[],
+    public incorrectClicks: { x: number, y: number }[],
+  ) { }
 }
