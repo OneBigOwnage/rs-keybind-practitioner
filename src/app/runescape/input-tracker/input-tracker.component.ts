4eebca7e8227c1e76c4593a2072894dbe1646ae8
Niek van den Bos <niekvandenbos@hotmail.com>
2023-06-30 01:15:21 +0200

    Implement input-tracker observables

--- src/app/runescape/input-tracker/input-tracker.component.ts
+++ src/app/runescape/input-tracker/input-tracker.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import TickRepository from '../TickRepository.service';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
-import { Tick, tickFactory } from '../Interactions';
+import { Observable, combineLatest } from 'rxjs';
+import { Interaction, MissedKeyPress, SuccessfulKeyPress, Tick } from '../Interactions';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-input-tracker',
@@ -10,11 +11,41 @@ import { Tick, tickFactory } from '../Interactions';
 })
 export class InputTrackerComponent implements OnInit {
 
-  public previousTicks$: Observable<Tick[]> = new BehaviorSubject([tickFactory('B CTRL-E A'), tickFactory('1 S D T')]);
-  public currentTick$: Observable<Tick> = new BehaviorSubject(tickFactory('B CTRL-E A'));
-  public upcomingTicks$: Observable<Tick[]> = new BehaviorSubject([tickFactory('1 S D T')]);
+  public previousTicks$: Observable<Tick[]>;
+  public currentTick$: Observable<Tick>;
+  public upcomingTicks$: Observable<Tick[]>;
 
-  constructor(protected repo: TickRepository) { }
+  constructor(protected repo: TickRepository) {
+    this.previousTicks$ = repo.ticks$().pipe(map(ticks => ticks.slice(-4, -1)));
+
+    this.currentTick$ = combineLatest([repo.ticks$(), repo.rotation$()])
+      .pipe(
+        filter(([ticks]) => ticks.length > 0),
+        map(([ticks, rotation]) => {
+          let index = ticks.length - 1;
+          let rotationTick = rotation[index];
+
+          return [ticks[index], rotationTick];
+        }),
+        map(([tick, rotationTick]) => {
+          if (!(rotationTick instanceof Tick)) {
+            return tick;
+          }
+
+          let interactions: Interaction[] = tick.interactions;
+          let indexToStartAddingShoulds = interactions.filter(i => i instanceof SuccessfulKeyPress || i instanceof MissedKeyPress).length;
+          let shoulds = rotationTick.interactions.slice(indexToStartAddingShoulds);
+
+          return new Tick(interactions.concat(shoulds));
+        }));
+
+    this.upcomingTicks$ = combineLatest([repo.ticks$(), repo.rotation$()])
+      .pipe(map(([ticks, rotation]) => {
+        let index = ticks.length;
+
+        return rotation.slice(index, index + 3);
+      }));
+  }
 
   ngOnInit(): void {
   }
