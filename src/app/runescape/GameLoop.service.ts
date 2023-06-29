bf3482144717bb7e76c6c7cc4023db96f966bef2
Niek van den Bos <niekvandenbos@hotmail.com>
2023-06-29 14:06:42 +0200

    Only include loop related code in the game loop

--- src/app/runescape/GameLoop.service.ts
+++ src/app/runescape/GameLoop.service.ts
@@ -1,146 +1,52 @@
-import { EventEmitter, Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
-import { ActualTick } from "./ActualTick";
-import KeypressPrettifier from "./KeypressPrettifier.service";
-import RotationRepository from "./RotationRepository.service";
+import { Injectable } from "@angular/core";
+import { Observable, Subject } from "rxjs";
 
 @Injectable({ providedIn: "root" })
-export default class GameLoop {
+export class GameLoop {
+  protected running: boolean = false;
+  protected tickInterval: number = 5000;
+  protected ticks: Subject<void> = new Subject();
 
-  public readonly tickLengthInMs: number = 600;
-
-  public ticks: BehaviorSubject<ActualTick[]> = new BehaviorSubject<ActualTick[]>([]);
-
-  public emitter = new EventEmitter<number>();
-
-  /**
-   * The epoch is the time when the game loop started.
-   */
-  protected epoch: number = performance.now();
-
-  /**
-   * The requestAnimationFrame ID.
-   */
-  protected requestID?: number;
-
-  /**
-   * We need a bound method to be able to remove the event listeners.
-   */
-  protected boundOnKeyPress = this.onKeyPress.bind(this);
-
-  /**
-   * We need a bound method to be able to remove the event listeners.
-   */
-  protected boundOnClick = this.onClick.bind(this);
-
-  constructor(protected prettifier: KeypressPrettifier, public repo: RotationRepository) { }
-
-  start() {
-    this.reset();
-    this.requestID = window.requestAnimationFrame(this.loop.bind(this));
-
-    this.registerEventListeners();
-  }
-
-  stop() {
-    this.unregisterEventListeners();
-
-    if (this.requestID !== undefined) {
-      window.cancelAnimationFrame(this.requestID);
-    }
-  }
-
-  reset() {
-    this.epoch = performance.now();
-    this.ticks = new BehaviorSubject<ActualTick[]>([]);
-  }
-
-  loop(timestamp: number) {
-    timestamp = timestamp - this.epoch;
-
-    let globalCoolDownLength = this.tickLengthInMs * 3;
-
-    this.emitter.emit(timestamp % globalCoolDownLength / globalCoolDownLength * 100);
-
-    let ticks = this.ticks.value;
-    let latestTick = ticks[ticks.length - 1];
-
-    if (ticks.length === 0 || timestamp - latestTick.timestamp >= this.tickLengthInMs) {
-      ticks.push(new ActualTick(this.hash(ticks.length), timestamp, [], []));
-
-      this.ticks.next(ticks);
-    }
-
-    this.requestID = window.requestAnimationFrame(this.loop.bind(this));
-  }
-
-  protected registerEventListeners() {
-    window.addEventListener("keydown", this.boundOnKeyPress);
-    window.addEventListener("click", this.boundOnClick);
-  }
-
-  protected unregisterEventListeners() {
-    window.removeEventListener("keydown", this.boundOnKeyPress);
-    window.removeEventListener("click", this.boundOnClick);
-  }
-
-  protected onKeyPress(event: KeyboardEvent) {
-    event.preventDefault();
-    event.stopPropagation();
-
-    let key = this.prettifier.extractKey(event);
-
-    if (!this.shouldRecordKeystroke(key, event)) {
+  public start() {
+    if (this.running) {
       return;
     }
 
-    let ticks = this.ticks.value;
-    let tick = this.ticks.value[ticks.length - 1];
-
-    tick.keyPresses.push(key);
-    ticks[ticks.length - 1] = tick;
-
-    this.ticks.next(ticks);
+    this.running = true;
+    this.gameLoop();
   }
 
-  protected onClick(event: MouseEvent) {
-    event.preventDefault();
-    event.stopPropagation();
-
-    if (this.ticks.value.length === 0) {
-      return;
-    }
-
-    let ticks = this.ticks.value;
-    let tick = this.ticks.value[this.ticks.value.length - 1];
-    tick.clicks.push({ x: event.clientX, y: event.clientY });
-    ticks[ticks.length - 1] = tick;
-
-    this.ticks.next(ticks);
+  public stop() {
+    this.running = false;
   }
 
-  protected shouldRecordKeystroke(key: string, event: KeyboardEvent): boolean {
-    if (this.ticks.value.length === 0) {
-      return false;
-    }
-
-    if (['Control', 'Alt', 'Shift'].includes(event.key)) {
-      return false;
-    }
-
-    return true;
+  public ticks$(): Observable<void> {
+    return this.ticks.asObservable();
   }
 
-  hash(index: number) {
-    // If the index exists in the repository, we use the existing ID.
-    // Otherwise, we create a new ID.
-    if (this.repo.rotation.value.length > index) {
-      console.log('Using existing ID.');
+  protected gameLoop() {
+    let lastTimestamp = performance.now();
+    let delta = 0;
 
-      return this.repo.rotation.value[index].ID;
+    const loop = (timestamp: number) => {
+      delta += timestamp - lastTimestamp;
+      lastTimestamp = timestamp;
+
+      while (delta >= this.tickInterval) {
+        delta -= this.tickInterval;
+        this.tick();
+      }
+
+      if (this.running) {
+        requestAnimationFrame(loop);
+      }
     }
 
-    return Math.random().toString(36).substring(2, 15)
-      + Math.random().toString(36).substring(2, 15);
+    this.tick();
+    requestAnimationFrame(loop);
+  }
+
+  protected tick() {
+    this.ticks.next();
   }
 }
