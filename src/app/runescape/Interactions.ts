8e1d24f6a1f1028ac492ba7f68bfaf02b4c86d8f
Niek van den Bos <niekvandenbos@hotmail.com>
2023-06-29 14:05:22 +0200

    Add interaction classes and types

---
+++ src/app/runescape/Interactions.ts
@@ -0,0 +1,37 @@
+
+export class ShouldPressKey {
+  constructor(public readonly key: string) { }
+}
+
+export class SuccessfulKeyPress {
+  constructor(public readonly key: string) { }
+}
+
+export class MissedKeyPress {
+  constructor(public readonly key: string) { }
+}
+
+export class UnexpectedKeyPress {
+  constructor(public readonly key: string) { }
+}
+
+export type Interaction =
+  ShouldPressKey
+  | SuccessfulKeyPress
+  | MissedKeyPress
+  | UnexpectedKeyPress;
+
+export type PlannedInteraction = ShouldPressKey;
+
+export type RecordedInteraction =
+  SuccessfulKeyPress
+  | MissedKeyPress
+  | UnexpectedKeyPress;
+
+export class Tick {
+  constructor (public interactions: Interaction[] = []) { }
+}
+
+export function tickFactory(input: string): Tick {
+  return new Tick(input.split('').map(key => new ShouldPressKey(key)));
+}
