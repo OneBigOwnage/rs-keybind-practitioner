011706cc8a037e7c10f5f2035e29f835f8dfd0fc
Niek van den Bos <niekvandenbos@hotmail.com>
2023-06-29 14:07:35 +0200

    Remove obsolete code.

--- src/app/runescape/ResultSet.ts
+++
@@ -1,9 +0,0 @@
-export default class ResultSet {
-  constructor(
-    public readonly ID: string,
-    public correctKeyPresses: string[],
-    public incorrectKeyPresses: string[],
-    public correctClicks: { x: number, y: number }[],
-    public incorrectClicks: { x: number, y: number }[],
-  ) { }
-}
