666ae733a45654d75c0eb7a4fd3972a6dc9b8db4
Niek van den Bos <niekvandenbos@hotmail.com>
2023-06-29 21:36:22 +0200

    Add new components, remove obsolete ones

--- src/app/runescape/runescape.module.ts
+++ src/app/runescape/runescape.module.ts
@@ -1,22 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RotationBuilderComponent } from './rotation-builder/rotation-builder.component';
-import { RotationComponent } from './rotation/rotation.component';
 import { FormsModule } from '@angular/forms';
 import { EditableTickComponent } from './editable-tick/editable-tick.component';
 import { TimeVisualizerComponent } from './time-visualizer/time-visualizer.component';
 import { PlannedTickComponent } from './planned-tick/planned-tick.component';
 import { ActualTickComponent } from './actual-tick/actual-tick.component';
 import { MatIconModule } from '@angular/material/icon';
+import { InputTrackerComponent } from './input-tracker/input-tracker.component';
+import { InputTrackerTickComponent } from './input-tracker-tick/input-tracker-tick.component';
 
 @NgModule({
   declarations: [
     RotationBuilderComponent,
-    RotationComponent,
     EditableTickComponent,
     TimeVisualizerComponent,
     PlannedTickComponent,
     ActualTickComponent,
+    InputTrackerComponent,
+    InputTrackerTickComponent,
   ],
   imports: [
     CommonModule,
@@ -25,11 +27,12 @@ import { MatIconModule } from '@angular/material/icon';
   ],
   exports: [
     RotationBuilderComponent,
-    RotationComponent,
     EditableTickComponent,
     TimeVisualizerComponent,
     PlannedTickComponent,
     ActualTickComponent,
+    InputTrackerComponent,
+    InputTrackerTickComponent,
   ],
 })
 export class RunescapeModule { }
