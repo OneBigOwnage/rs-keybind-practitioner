6efb9cffafb4d8ace9b80f2ec7db6d3ec4279b9a
Niek van den Bos <niekvandenbos@hotmail.com>
2023-06-29 21:36:58 +0200

    Load more realistic rotation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import Game from './runescape/Game.service';
-import { ShouldPressKey, Tick, tickFactory } from './runescape/Interactions';
+import { tickFactory } from './runescape/Interactions';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +10,6 @@ import { ShouldPressKey, Tick, tickFactory } from './runescape/Interactions';
 export class AppComponent implements OnInit {
 
   showRotationBuilder: boolean = false;
-
   percentage = 0;
 
   constructor(public game: Game) {
@@ -21,11 +20,48 @@ export class AppComponent implements OnInit {
   }
 
   loadRotation() {
+    // This is the telos p1 rotation with my new 2023 binds
     this.game.setRotation([
-      tickFactory('AABAAB'),
-      tickFactory('BB'),
-      tickFactory(' '),
+      tickFactory('B CTRL-E A'),
+      tickFactory(),
+      tickFactory(),
+      tickFactory('1 S D T'),
+      tickFactory(),
+      tickFactory(),
+      tickFactory('ALT-3 ALT-B X A'),
+      tickFactory(),
+      tickFactory(),
+      tickFactory('V S D'),
+      tickFactory(),
+      tickFactory(),
+      tickFactory('H'),
+      tickFactory(),
+      tickFactory(),
+      tickFactory('Z A'),
+      tickFactory(),
+      tickFactory(),
+      tickFactory('2 S D T'),
+      tickFactory(),
+      tickFactory(),
+      tickFactory('A Y'),
+      tickFactory(),
+      tickFactory(),
+      tickFactory('2 3'),
+      tickFactory(),
+      tickFactory(),
       tickFactory('C'),
+      tickFactory(),
+      tickFactory(),
+      tickFactory('1 S D T'),
+      tickFactory(),
+      tickFactory(),
+      tickFactory('A CTRL-W'),
+      tickFactory(),
+      tickFactory(),
+      tickFactory('CTRL-Q'),
+      tickFactory(),
+      tickFactory('5'),
+      tickFactory('Y'),
     ]);
   }
 }
