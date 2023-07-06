090f8a414389e436f0c0de4e5962315557f4e56a
Niek van den Bos <niekvandenbos@hotmail.com>
2023-07-06 19:26:13 +0200

    Mark keybinds required in the current rotation

--- src/app/runescape/keybind-configurator/keybind-configurator.component.ts
+++ src/app/runescape/keybind-configurator/keybind-configurator.component.ts
@@ -4,7 +4,9 @@ import { InputHandler } from '../InputHandler.service';
 
 import { RunescapeAction, RunescapeActions } from '../RunescapeActions';
 import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { RotationRepository } from '../rotation-repository.service';
+import TickRepository from '../TickRepository.service';
 
 @Component({
   selector: 'app-keybind-configurator',
@@ -16,7 +18,7 @@ export class KeybindConfiguratorComponent implements OnInit, OnDestroy {
 
   protected actionTarget: RunescapeAction | undefined;
 
-  constructor(public repo: KeybindRepository, public input: InputHandler) {
+  constructor(public repo: KeybindRepository, public input: InputHandler, public tickRepo: TickRepository) {
     input.keyPresses$().subscribe(event => {
       if (this.actionTarget) {
         this.repo.storeOrUpdate(keybindFactory(event.key, this.actionTarget));
@@ -42,11 +44,15 @@ export class KeybindConfiguratorComponent implements OnInit, OnDestroy {
     this.actionTarget = undefined;
   }
 
-  public getKeybindForAction$(action: string): Observable<string> {
+  public getKeybindForAction$(action: RunescapeAction): Observable<string> {
     return this.repo.keybinds$().pipe(map(keybinds => keybinds.find(keybind => keybind.ability.name === action)?.keyCombination || ''));
   }
 
   public clearKeybindForAction(action: RunescapeAction) {
     this.repo.delete(keybindFactory('', action));
+  }
+
+  public isUsedInCurrentRotation$(action: RunescapeAction): Observable<boolean> {
+    return this.tickRepo.rotation$().pipe(map(rotation => rotation.ticks.some(tick => tick.actions.some(tickAction => tickAction.name === action))));
   }
 }
